9971764ef1ac3706c02b14cf30d6fe7a0f768fc4
kyven <kyvenwu1@gmail.com>
2024-10-05 23:16:46 -0500

    move build to zebar config dir

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,8 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      pages: "build",
-      assets: "build",
+      pages: "../neobrutal-zebar-build",
+      assets: "../neobrutal-zebar-build",
       fallback: undefined,
       precompress: false,
       strict: true
